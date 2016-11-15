26ce1ea7b1aea4da39df0c9c8020513898802570
Delyer <jiayaoit@126.com>
2016-11-14 16:04:05 -0800

    Initial Commit

---
+++ JYCarousel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYCarousel
+//
+//  Created by Dely on 16/11/14.
+//  Copyright © 2016年 Dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYCarousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYCarousel
+//
+//  Created by Dely on 16/11/14.
+//  Copyright © 2016年 Dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
