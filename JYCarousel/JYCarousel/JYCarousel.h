79c617b556acbeb550fc018ba63c3f0b1185025b
jiayao <jiayao@jiuxian.com>
2016-11-17 22:15:38 +0800

    添加作者相关信息

--- JYCarousel/JYCarousel/JYCarousel.h
+++ JYCarousel/JYCarousel/JYCarousel.h
@@ -6,6 +6,20 @@
 //  Copyright © 2016年 Dely. All rights reserved.
 //
 
+/*
+ * 这是一个使用起来非常简单的开源库，自带下载缓存，不会造成循环引用，不用考虑定时器不会销毁（我都已经帮你做好了）
+ * 如果使用过程中有任何问题，可以联系我，我会第一时间回复你，然后尽我的努力把这个库维护的更好，更简单方便的使用。
+ * 谢谢支持，如果喜欢，请给我个star。
+ *
+ * 作者：Delyer
+ * 简书：http://www.jianshu.com/users/e95705fe04d9    （现在写博客的主要地方）
+ * 个人博客：http://dely.vip
+ * 新浪博客：http://blog.sina.com.cn/u/1721109400
+ * github：https://github.com/Delyer
+ * 邮箱：jiayaoit@126.com
+ * 微信：Delyer521
+ */
+
 #import <UIKit/UIKit.h>
 #import "JYConfiguration.h"
 #import "JYPageControl.h"
