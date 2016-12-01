dbbf4593be29ea878c2f19763a5ce8c4bf80ce27
Delyer <jiayaoit@126.com>
2016-11-30 17:18:18 -0800

    修改宽度

--- JYCarousel/ViewController/DemoViewController.m
+++ JYCarousel/ViewController/DemoViewController.m
@@ -87,7 +87,7 @@
     NSMutableArray *imageArray2 = [[NSMutableArray alloc] initWithArray: @[@"https://p1.bqimg.com/524586/894925a41a745ba8.jpg",@"https://p1.bqimg.com/524586/edd59898ac21642f.jpg",@"https://p1.bqimg.com/524586/d277aa654cd60c3d.jpg",@"https://p1.bqimg.com/524586/a49b8d3e1b953f25.jpg",@"https://p1.bqimg.com/524586/972bff3b7a5fb7e1.jpg"]];
     
     if (!_carouselView2) {
-        _carouselView2 = [[JYCarousel alloc] initWithFrame:CGRectMake(0, 120, ViewWidth(self.view), 100) configBlock:^JYConfiguration *(JYConfiguration *carouselConfig) {
+        _carouselView2 = [[JYCarousel alloc] initWithFrame:CGRectMake(0, 120, SCREEN_WIDTH, 100) configBlock:^JYConfiguration *(JYConfiguration *carouselConfig) {
             carouselConfig.pageContollType = MiddlePageControl;
             carouselConfig.pageTintColor = [UIColor whiteColor];
             carouselConfig.currentPageTintColor = [UIColor redColor];
